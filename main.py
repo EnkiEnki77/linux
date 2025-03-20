d2e4b9a385128ff61e53da4d56b2b590faee14c7
EnkiEnki77 <layman212@gmail.com>
2025-03-20 18:33:10 -0400

    Learned the difference between terminal and shell/cli

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-# hi
+# To print a string to the console run the command echo "something"
+
+# To execute a file in python: python file.py
+
+# Shell, CLI, and Terminal often refer to the same thing, a program utilized for issuing text-based
+# commands.
+# Pedantically though the terminal is responsible just for drawing text on the screen and processing
+# key strokes, the CLI or Shell is the program that handles the commands.
