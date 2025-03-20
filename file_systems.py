03be1241f02bc3b2a412e9efb4d05b84923b3f1b
EnkiEnki77 <layman212@gmail.com>
2025-03-20 19:17:56 -0400

    Learned about ls and cd commands

---
+++ file_systems.py
@@ -0,0 +1,24 @@
+# All the data stored on your computer is organized into files and directories, which are organized
+# into a tree-like structure called the filesystem.
+
+# Directories are containers that hold files and other directories
+# Files are dumps of raw binary data, they can represent anything, images, text, videos, etc.
+
+# At the top of the file system is a single directory, called the root directory.
+
+# When you open the terminal your working directory (the one youre currently in) will likely be your home
+# directory.
+
+# The pwd command prints the filepath to your working directory.
+
+# A file path is a string that describes the location of a file or directory.
+
+# /home/remdu
+# / is the root directory, home is the name of a directory inside the root, /remdu is a directory inside
+# /home, and is the working directory since its the last directory in the file path.
+
+
+# The ls command lets you see the contents of your current working directory
+
+
+# The cd command lets you go to a directory in your working directory.
