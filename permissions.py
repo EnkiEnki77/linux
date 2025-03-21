27059a10e19983f0b6026d69f1a8976e48112463
EnkiEnki77 <layman212@gmail.com>
2025-03-21 03:24:47 -0400

    Learned sudo command

---
+++ permissions.py
@@ -0,0 +1,3 @@
+# unix based systems support multiple users, each with their own home directory, files, and permissions
+
+# sudo lets you run a command as a superuser. You need a password with superuser privileges.
