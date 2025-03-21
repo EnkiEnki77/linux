88075286ded22c58dfad1262f60c2a5e0d8febdf
EnkiEnki77 <layman212@gmail.com>
2025-03-21 03:33:10 -0400

    Learned about directory and file permissions

--- permissions.py
+++ permissions.py
@@ -1,3 +1,22 @@
 # unix based systems support multiple users, each with their own home directory, files, and permissions
 
 # sudo lets you run a command as a superuser. You need a password with superuser privileges.
+
+
+# Permissions determine who can do what to files and directories. Theyre generally presented as a 10-
+# character string.
+
+# Anyone accessing a file or directory may or may not have permissions to read, write, or execute it.
+
+# The first character of permissions will be a d or -, indicating directory or file respectively.
+
+# Next three characters are rwx, read, write, execute permissions for the owner of the file. Generally
+# the one who created the file, but this can be changed manually.
+
+# If the permission is granted the letter is present, if its not granted itll be a -.
+
+# The next 3 characters apply to permissions for the group
+
+# The last 3 are permissions for everyone else.
+
+
