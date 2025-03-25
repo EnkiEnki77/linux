2c2bf7dc457a6ef3ca1377e98231c5338306f7f2
remdu <layman212@gmail.com>
2025-03-24 21:14:24 -0400

    Learned how to edit a file with neovim

---
+++ package_managers.py
@@ -0,0 +1,29 @@
+# A package manager is a software tool that lets you install other software.
+
+# It lets you:
+# Download software from official sources
+# Install software
+# Update software
+# Remove software
+# Manage dependencies
+
+# APT Advanced Package Tool is the primary package manager for Ubuntu
+
+
+
+# To edit a file with neo vim run:
+# nvim fil_name.txt
+
+# go to the line you want to edit
+
+# press i to enter insert mode
+
+# edit the line
+
+# press esc to go back to normal mode
+
+# Save changes with:
+# :w
+
+# Exit neovim:
+# :q
